92677193d0bf08b2e9126bfd3d5a1163f634d362
themavencoder <eniolaplus08@gmail.com>
2020-10-04 21:25:31 +0100

    Configuration, themes and first post

---
+++ content/articles/Firstpost.md
@@ -0,0 +1,10 @@
+Title: My super title
+Date: 2010-12-03 10:20
+Modified: 2010-12-05 19:30
+Category: abc
+Tags: abc, xyz
+Slug: my-super-post
+Authors: Tobiloba Adejumo
+Summary: Short description
+
+This is the content of my super blog post.
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Tobiloba Adejumo'
+SITENAME = 'Tobiloba Adejumo'
+SITEURL = ''
+SITETITLE = AUTHOR
+SITESUBTITLE = 'A random blog on applications of machine learning in healthcare' 
+SITEDESCRIPTION = 'A random blog on the applications of machine learning in healthcare' 
+PATH = 'content'
+
+TIMEZONE = 'Africa/Lagos'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+THEME = 'themes/Flex' 
+BROWSER_COLOR = '#333333'
+PYGMENTS_STYLE = 'default' 
+SITELOGO = '/images/profile.png'
+
+# Blogroll
+LINKS = (('Website', 'tobilobaadejumo.com'),
+         ('Medium Blog', 'medium.com/@themavencoder'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'twitter.com/themavencoder'),
+          ('LinkedIn', 'linkedin.com/in/tobilobaadejumo'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
